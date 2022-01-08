9b963135521fba041ca6fe34f4bf4d6da4f340b0
Irina <livefortoday@mail.ru>
2022-01-08 12:10:04 +0300

    personalMovieDB practice

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
 "use strict";
 
-const category = "toys";
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-console.log(`http://someurl.com/${category}/5`);
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	private: false,
+};
 
-const user = "Ivan";
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+		b = prompt('На сколько оцените его?', ''),
+		c = prompt('Один из последних просмотренных фильмов?', ''),
+		d = prompt('На сколько оцените его?', '');
 
-alert(`Hello, ${user}`);
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;	
+
+console.log(personalMovieDB);
